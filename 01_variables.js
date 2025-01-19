0c804086636bb7c279d609ab608cc8f3e04b76df
Jatin-35 <jtjasrotia123@gmail.com>
2025-01-19 12:00:00 +0530

    Commit for Learning-Flask on Febuary 01,  2025

---
+++ 01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 14453
+let accountEmail = "jatin@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState ;
+
+
+// accountId = 2  // Not allowed
+
+
+accountEmail = "hc@gmail.com"
+var accountPassword = "32112121"
+accountCity = "Jammu"
+
+console.log(accountId);
+
+/*
+prefer to not use var
+beacause of issue in block space and functional space
+
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity , accountState])
+
