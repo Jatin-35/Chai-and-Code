869e6bc44c6c16f6b79571de75d41c4a8a07fdec
Jatin-35 <jtjasrotia123@gmail.com>
2025-02-01 12:00:00 +0530

    Commit for Chai-and-Code on Febuary 01,  2025

---
+++ 03_conversionOperation.js
@@ -0,0 +1,83 @@
+// let score = undefined
+
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "333abc" => NaN , but type is number
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) // true
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+// [] , {}=> true
+// -1 => true
+
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber) // "33"
+// console.log(typeof stringNumber)
+
+
+// ************************** Operations ******************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue) // -3
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2%2)
+// console.log(2/2)
+
+
+// let str1 = "hello "
+// let str2 = "world"
+// console.log(str1 + str2) // "hello world"
+
+// console.log(str1 - str2) // NaN
+
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)  
+// console.log(1 + 2 + "2")  
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+// 
+
+let x = 3;
+const y = ++x;
+// x is 4; y is 4
+
+let x2 = 3n;
+const y2 = ++x2;
+console.log(x2 , y2)
+// x2 is 4n; y2 is 4n
+
+let x1= 3;
+const y1 = x++;
+// x is 4; y is 3
+
+let x22 = 3n;
+const y22 = x22++;
+console.log(x22 , y22)
+// x2 is 4n; y2 is 3n
+
