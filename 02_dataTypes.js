15628fdeaf51d5c6c5b98a34d98a21d492c1c838
Jatin-35 <jtjasrotia123@gmail.com>
2025-05-02 00:00:00 +0530

    Commit for Learning-Flask on Febuary 01,  2025

---
+++ 02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node js ,not browser
+
+console.log(3
+    +
+    3); // code readibility should be high
+
+console.log("Hitesh")
+
+let name = "jatin"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined => 
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof null) // null is an object(datatype)
+console.log(typeof undefined)  // undefined datatype is undefined
